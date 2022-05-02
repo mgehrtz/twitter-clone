6235308cb2edae9a7624e8c5f1cd2f040ca3e4e5
Michael Gehrtz <63627744+mgehrtz@users.noreply.github.com>
2022-05-02 15:20:50 -0500

    Add mailer config.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.perform_deliveries = false
+ActionMailer::Base.delivery_method = :file
